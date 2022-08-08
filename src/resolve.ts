551630876aa4766a8c4a86024a7ef36727d88e6c
Yentl Deuster <yentlclaudia.deuster@student.hs-rm.de>
2022-08-08 15:28:53 +0200

    test

--- src/resolve.ts
+++ src/resolve.ts
@@ -106,17 +106,17 @@ export const resolveNode = (
         case 'greater-than':
         case 'greater-than-equals':
         case 'approximates':
-            if (node.b.type === 'number'){
-                let x = ''
-                x = x + node.b
-                return {
-                    type: 'number',
-                    value: parseFloat(x),
-                }
+            //if (node.b.type === 'number'){
+            let x = ''
+            x = x + node.b
+            return {
+                type: 'number',
+                value: parseFloat(x),
             }
+            //}
             
             //console.log();
-            throw new ResolverError('noComparison', node, {})
+            //throw new ResolverError('noComparison', node, {})
         case 'matrix': {
             // Keep track of resolved unit
             let unit: UnitLookup | null = null
