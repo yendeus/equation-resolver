5b359594e77daef0580267978bd6fb225d01d1f4
Yentl Deuster <yentlclaudia.deuster@student.hs-rm.de>
2022-08-08 15:36:17 +0200

    intendation?

--- src/resolve.ts
+++ src/resolve.ts
@@ -107,14 +107,14 @@ export const resolveNode = (
         case 'greater-than-equals':
         case 'approximates': {
             //if (node.b.type === 'number'){
-        let x = ''
-        x = x + node.b
-        return {
-            type: 'number',
-            value: parseFloat(x),
-        }
-            //}
-        }
+                let x = ''
+                x = x + node.b
+                return {
+                    type: 'number',
+                    value: parseFloat(x),
+                }
+                //}
+            }
             //console.log();
             //throw new ResolverError('noComparison', node, {})
         case 'matrix': {
